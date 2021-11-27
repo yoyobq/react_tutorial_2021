8cbdbed34f50014f364780566cd69c292fedf75b
Bu Qiang@SSTS <yoyobq@hotmail.com>
2021-11-27 21:27:57 +0800

    更新 Gallery 组件，演示同一组件内设置多个 state 变量。

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -4,9 +4,14 @@ import { sculptureList } from './data.js';
 function Gallery() {
   // let index = 0;
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
   function handleClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
@@ -22,13 +27,16 @@ function Gallery() {
       <h3>  
         ({index + 1} of {sculptureList.length})
       </h3>
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <div>
+        <img 
+          src={sculpture.url} 
+          alt={sculpture.alt}
+        />
+      </div>
     </>
   );
 }
